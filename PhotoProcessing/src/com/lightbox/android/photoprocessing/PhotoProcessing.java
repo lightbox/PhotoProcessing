c76a562dbd518e6616cd81f86597e5dab4df789e
Nilesh Patel <nilesh@lightbox.com>
2012-05-15 17:33:55 +0100

    Added more licence info into some source files.

--- PhotoProcessing/src/com/lightbox/android/photoprocessing/PhotoProcessing.java
+++ PhotoProcessing/src/com/lightbox/android/photoprocessing/PhotoProcessing.java
@@ -1,6 +1,19 @@
-/**
- * Copyright (c) 2012 Lightbox
+/*
+ * Copyright (C) 2012 Lightbox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.lightbox.android.photoprocessing;
 
 import android.graphics.Bitmap;
@@ -8,7 +21,7 @@ import android.graphics.Bitmap.Config;
 
 /** 
  * PhotoProcessing 
- * @author nilesh
+ * @author Nilesh Patel
  */
 public class PhotoProcessing {
 	/** Used to tag logs */
--- PhotoProcessing/src/com/lightbox/android/photoprocessing/utils/BitmapUtils.java
+++ PhotoProcessing/src/com/lightbox/android/photoprocessing/utils/BitmapUtils.java
@@ -1,6 +1,19 @@
-/**
- * Copyright (c) 2012 Lightbox
+/*
+ * Copyright (C) 2012 Lightbox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.lightbox.android.photoprocessing.utils;
 
 import android.graphics.Bitmap;
@@ -10,7 +23,7 @@ import android.util.FloatMath;
 
 /** 
  * BitmapUtils 
- * @author nilesh
+ * @author Nilesh Patel
  */
 public class BitmapUtils {
 	/** Used to tag logs */
